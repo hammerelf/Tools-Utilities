07d93edf0c31e1c6f8e4ad51378fa77251dbc6cf
Ryan King <rking2789@gmail.com>
2024-04-08 11:59:44 -0500

    Created singleton script template

---
+++ ScriptTemplates/80-C# Singleton-NewSingletonScript.cs
@@ -0,0 +1,11 @@
+//Created by: 
+
+using HammerElf.Tools.Utilities;
+using UnityEngine;
+
+    #ROOTNAMESPACEBEGIN#
+public class #SCRIPTNAME# : Singleton<#SCRIPTNAME#>
+{
+    
+}
+#ROOTNAMESPACEEND#
