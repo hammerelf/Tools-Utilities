8bf9f9ce0a82b51893226f9a3b179e6b6d2dea4c
Ryan King <ryan@thedevhouseagency.com>
2024-04-10 15:35:33 -0500

    Script to auto import package

--- Editor/AssetImportPostProcessor.cs
+++ Editor/AssetImportPostProcessor.cs
@@ -5,29 +5,36 @@ using UnityEngine;
 using System.IO;
 using HammerElf.Tools.Utilities;
 
-public class AssetImportPostProcessor /*: AssetPostprocessor*/
+public class AssetImportPostProcessor : AssetPostprocessor
 {
-    //private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
-    //{
-    //    if (!AssetDatabase.IsValidFolder("Assets/ScriptTemplates"))
-    //    {
-    //        AssetDatabase.CreateFolder("Assets", "ScriptTemplates");
-    //    }
+    private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        foreach (string assetPath in importedAssets)
+        {
+            if(assetPath.StartsWith("Packages/com.hammerelf.tools.utilities/UnityPackages") && assetPath.EndsWith(".unitypackage"))
+            {
+                AssetDatabase.ImportPackage(assetPath, false);
+            }
+        }
+        //if (!AssetDatabase.IsValidFolder("Assets/ScriptTemplates"))
+        //{
+        //    AssetDatabase.CreateFolder("Assets", "ScriptTemplates");
+        //}
 
-    //    // Loop through all imported assets
-    //    foreach (string assetPath in importedAssets)
-    //    {
-    //        if (assetPath.EndsWith(".txt"))
-    //        {
-    //            string destinationPath = "Assets/ScriptTemplates/" + Path.GetFileName(assetPath);
+        //// Loop through all imported assets
+        //foreach (string assetPath in importedAssets)
+        //{
+        //    if (assetPath.EndsWith(".txt"))
+        //    {
+        //        string destinationPath = "Assets/ScriptTemplates/" + Path.GetFileName(assetPath);
 
-    //            FileUtil.CopyFileOrDirectory(assetPath, destinationPath);
-    //            ConsoleLog.Log("Moved text file: " + assetPath + " to " + destinationPath);
-    //        }
-    //    }
+        //        FileUtil.CopyFileOrDirectory(assetPath, destinationPath);
+        //        ConsoleLog.Log("Moved text file: " + assetPath + " to " + destinationPath);
+        //    }
+        //}
 
-    //    //Should be this scripts location
-    //    AssetDatabase.DeleteAsset("Packages/com.hammerelf.tools.utilities/Editor/AssetImportPostProcessor.cs");
-    //    AssetDatabase.Refresh();
-    //}
+        ////Should be this scripts location
+        //AssetDatabase.DeleteAsset("Packages/com.hammerelf.tools.utilities/Editor/AssetImportPostProcessor.cs");
+        //AssetDatabase.Refresh();
+    }
 }
